2d9b1b1ce660b03164dea4b2e4c24d6ec72b6ade
AnthonyNguyenTran <67623559+AnthonyNguyenTran@users.noreply.github.com>
2020-07-02 19:35:50 +1000

    Update TheProgram.py

--- TheProgram.py
+++ TheProgram.py
@@ -1,27 +1,47 @@
 import psycopg2
+import argparse
 
-# connect to the db
-con = psycopg2.connect(
-            host = "localhost",
-            database = "postgres",
-            user = "postgres",
-            password = "4321"
-)
+if __name__ == '__main__':
+    # Initialize the parser
+    parser = argparse.ArgumentParser()
 
+    # Add the parameters positional/optional
+    parser.add_argument('Command', help="Command to display list or insert into list")
+    parser.add_argument('ThePartID', help="The part id to insert", nargs='?', default='null')
+    parser.add_argument('ThePartName', help="The part name to insert", nargs='?', default='null')
+    parser.add_argument('ThePartBrand', help="The part brand to insert", nargs='?', default='null')
+    parser.add_argument('ThePartPrice', help="The part price to insert", nargs='?', default='null')
 
-cur = con.cursor()
-cur.execute("insert into pc_parts (part_id, part_name, brand, price) values (112, 'Storage', 'Dell, 49.99)")
-cur.execute("select part_id, part_name, brand, price from pc_parts")
+    # Parse the arguments
+    args = parser.parse_args()
 
-rows = cur.fetchall()
+    # connect to the db
+    con = psycopg2.connect(
+        host="localhost",
+        database="postgres",
+        user="postgres",
+        password="4321"
+    )
 
-for r in rows:
-    print (f"Part_ID: {r[0]} Part_Name: {r[1]} Brand: {r[2]} Price: {r[3]}")
+    cur = con.cursor()
 
+    if args.Command == 'list':
+        cur.execute("select part_id, part_name, brand, price from pc_parts")
+        rows = cur.fetchall()
 
-con.commit()
+        for r in rows:
+            print(f"Part_ID: {r[0]} Part_Name: {r[1]} Brand: {r[2]} Price: {r[3]}")
 
-cur.close()
+    if args.Command == 'add':
+        the_part_id = int(args.ThePartID)
+        the_part_name = args.ThePartName
+        the_part_brand = args.ThePartBrand
+        the_part_price = float(args.ThePartPrice)
+        cur.execute("insert into pc_parts (part_id, part_name, brand, price) values (%s, %s, "
+                    "%s, %s)", (the_part_id, the_part_name, the_part_brand, the_part_price))
+        con.commit()
 
-#close the connection
-con.close()
+    cur.close()
+
+    # close the connection
+    con.close()
